8ca31d790154afd920c17e998cdd609a0dc8ce96
David J Williamson <davidjwilliamson@davids-mbp.clients.soton.ac.uk>
2019-10-04 09:58:19 +0100

    bigger comment

--- arrow_plot.py
+++ arrow_plot.py
@@ -5,6 +5,8 @@ def arrow_plot(ax,x,y,color='blue',quiver_thickness=0.005,edgecolor=None,facecol
     
     Oddly, `plot` uses a different scale for `linewidth` than `quiver` does for `width`, so this just uses `quiver` twice - once
     for the ``background'' lines, and once to place arrows in the middle. Thickness and color are set with kwargs.
+    
+    You can plot this on an axis/subplot, or you can pass `matplotlib.pyplot` or whatever and it'll work too
     """
     if edgecolor is None:
         edgecolor=color
